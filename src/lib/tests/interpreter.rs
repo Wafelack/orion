df6c4687698a27962261d31ebef97c643d5e998e
Wafelack <wafelack@protonmail.com>
2021-02-02 10:40:31 +0100

    :white_check_mark: Writing tests for static_cast

--- src/lib/tests/interpreter.rs
+++ src/lib/tests/interpreter.rs
@@ -289,6 +289,19 @@ mod test {
                 interpreter.eval()?;
                 Ok(())
             }
+
+            #[test]
+            fn static_cast() -> crate::Result<()> {
+                let code = "(define a \"true\"); string\n(define casted (static_cast \"bool\" a))(assert casted)";
+
+                let mut lexer = Lexer::new(code.to_owned());
+                let toks = lexer.scan_tokens();
+                let ast = Parser::new(toks).parse_tokens()?;
+                let mut interpreter = Interpreter::new(ast, vec![]);
+
+                interpreter.eval()?;
+                Ok(())
+            }
         }
     }
 }
