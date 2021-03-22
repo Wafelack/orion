85acc4d5959f713a4ef5044440ae8361bec5a250
Wafelack <wafelack@protonmail.com>
2021-03-22 18:08:17 +0100

    core: Writing parser.

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
 use crate::{Result, error, OrionError, lexer::{Token, TType}};
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum NType {
     Ident(String),
     Integer(i32),
@@ -8,6 +9,7 @@ pub enum NType {
     Bool(bool),
 }
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct Node {
     pub ntype: NType,
     pub children: Vec<Node>,
@@ -23,6 +25,24 @@ impl Node {
     pub fn add_child(&mut self, child: Node) {
         self.children.push(child);
     }
+    pub fn stringify(&self, indentations: usize) -> String {
+        let mut toret = String::new();
+        toret.push_str(&format!("\n{}[\n", gen_indents(indentations)));
+        for children in &self.children {
+            toret.push_str(&format!("{}{{\n", gen_indents(indentations + 1)));
+            toret.push_str(&format!("{}@type : ", gen_indents(indentations + 2)));
+            toret.push_str(&format!("{:?},\n", children.ntype));
+
+            if !children.children.is_empty() {
+                toret.push_str(&format!("{}@children : ", gen_indents(indentations + 2)));
+                toret.push_str(&children.stringify(indentations + 3));
+            }
+            toret.push_str(&format!("{}}},\n", gen_indents(indentations + 1)))
+        }
+        toret.push_str(&format!("{}],\n", gen_indents(indentations - 1)));
+        toret
+    }
+
 }
 
 pub struct Parser {
@@ -49,7 +69,7 @@ impl Parser {
     }
 
     fn proc_fn(&mut self) -> Result<Node> {
-        let raw_tp = self.advance().unwrap();
+        let raw_tp = self.advance().unwrap().clone();
         let mut to_push = match &raw_tp.ttype {
             TType::LParen => self.proc_fn()?,
             TType::Ident(i) => Node::new(NType::Ident(i.to_string())),
@@ -61,18 +81,64 @@ impl Parser {
         while !self.is_at_end() {
             let raw = self.advance().unwrap();
 
-            if raw.ttype == TType::RParen {
-                closed = true;
-                break;
-            }
-
-            match raw.ttype {
-
+            match &raw.ttype {
+                TType::LParen => to_push.add_child(self.proc_fn()?),
+                TType::Str(s) => to_push.add_child(Node::new(NType::Str(s.to_string()))),
+                TType::Number(n) => to_push.add_child(Node::new(NType::Integer(*n))),
+                TType::Float(r) => to_push.add_child(Node::new(NType::Single(*r))),
+                TType::Bool(b) => to_push.add_child(Node::new(NType::Bool(*b))),
+                TType::Ident(i) => to_push.add_child(Node::new(NType::Ident(i.to_string()))),
+                TType::RParen => {
+                    closed = true;
+                    break;
+                }
             }
 
 
         }
 
+        if !closed && self.input[self.current - 1].ttype != TType::RParen {
+            return error!("{}:{} | Unclosed expression, expected ')'", raw_tp.line, raw_tp.col);
+        }
+
+        Ok(to_push)
+    }
+
+    fn parse_tokens(&mut self) -> Result<()> {
+        let tok = self.advance().unwrap();
+
+        match tok.ttype {
+            TType::LParen => {
+                let to_add = self.proc_fn()?;
+                self.output.add_child(to_add);
+                Ok(())
+            }
+
+            _ => error!("{}:{} | Expected Opening Parenthese, found {}.", tok.line, tok.col, tok.get_type()),
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Node> {
+        while !self.is_at_end() {
+            self.parse_tokens()?;
+        }
+
         Ok(self.output.clone())
+    }
+}
+
+
+fn gen_indents(amount: usize) -> String {
+    let mut toret = String::new();
+    for _ in 0..amount {
+        toret.push_str("  ");
+    }
+    toret
+}
+
+impl std::fmt::Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.stringify(1))?;
+        Ok(())
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-    use crate::{Result, lexer::{Lexer, TType, Token}};
+    use crate::{Result, parser::{Parser, NType, Node}, lexer::{Lexer, TType, Token}};
 
 #[test]
     fn lexing() -> Result<()> {
@@ -9,6 +9,38 @@ mod test {
 
         assert_eq!(tokens,
                    vec![Token::new(TType::LParen, 1, 1), Token::new(TType::RParen, 2, 1), Token::new(TType::Str("Foo Bar".to_owned()), 11, 1), Token::new(TType::Bool(true), 15, 1), Token::new(TType::Bool(false), 21, 1), Token::new(TType::Ident("TrueFalse".to_owned()), 31, 1), Token::new(TType::Number(4), 33, 1), Token::new(TType::Float(9.1), 37, 1)]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn parsing() -> Result<()> {
+        let code = "(let foo (lambda (x y) (+ x y)))(while x y)";
+        let tokens = Lexer::new(code).proc_tokens()?;
+        let node = Parser::new(tokens).parse()?;
+
+        let mut master = Node::new(NType::Ident("begin".to_owned()));
+        let mut let_func = Node::new(NType::Ident("let".to_owned()));
+        let mut lambda_func = Node::new(NType::Ident("lambda".to_string()));
+        let mut x = Node::new(NType::Ident("x".to_string()));
+        x.add_child(Node::new(NType::Ident("y".to_string())));
+        let mut plus = Node::new(NType::Ident("+".to_string()));
+        plus.add_child(Node::new(NType::Ident("x".to_string())));
+        plus.add_child(Node::new(NType::Ident("y".to_string())));
+
+        lambda_func.add_child(x);
+        lambda_func.add_child(plus);
+        let_func.add_child(Node::new(NType::Ident("foo".to_owned())));
+        let_func.add_child(lambda_func);
+
+        let mut while_func = Node::new(NType::Ident("while".to_owned()));
+        while_func.add_child(Node::new(NType::Ident("x".to_string())));
+        while_func.add_child(Node::new(NType::Ident("y".to_string())));
+        
+        master.add_child(let_func);
+        master.add_child(while_func);
+
+        assert_eq!(node, master);
 
         Ok(())
     }
