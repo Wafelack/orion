f864765394732157938dc3fc93e3fae427f20e87
Wafelack <wafelack@protonmail.com>
2021-03-25 14:27:26 +0100

    core: Wiring up interpreter.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,10 +1,66 @@
-use crate::{parser::Node};
-use std::collections::BTreeMap;
+use crate::{error, OrionError, Result, parser::{Node, NType}};
+use std::collections::HashMap;
 
 pub enum Value {
     Integer(i32),
     Single(f32),
     Boolean(bool),
     Str(String),
-    Function(Node, Vec<String>, Vec<BTreeMap<String, Value>>)
+    Maybe(Option<Value>),
+    Function(Node, Vec<String>, Vec<HashMap<String, Value>>)
+}
+
+impl Value {
+    pub fn get_type()
+}
+
+pub struct Interpreter {
+    pub input: Node,
+    pub scopes: Vec<HashMap<String, Value>>,
+}
+
+impl Interpreter {
+    pub fn new(ast: Node) -> Self {
+        Self {
+            scopes: vec![HashMap::new()],
+            input: ast,
+        }
+    }
+    fn process_ast(&mut self, ast: &Node) -> Result<Value> {
+        self.eval_calls(&ast.children)
+    }
+    pub fn eval(&mut self) -> Result<Value> {
+        let ast = self.input.clone();
+        self.eval_calls(&ast.children)
+    }
+    fn eval_calls(&mut self, calls: &Vec<Node>) -> Result<Value> {
+        for (idx, call) in calls.iter().enumerate() {
+            if let NType::Ident(i) = &call.ntype {
+                if idx == calls.len() - 1 {
+                    return self.eval_call(&call);
+                } else if i == "return" {
+                    return self.eval_call(&call);
+                } else {
+                    self.eval_call(&call)?;
+                }
+            } else {
+                return error!("This is a bug, please report it with the following information: INVALID_IDENTIFIER: [{}:{}]", file!(), line!());
+            }
+        }
+
+        error!("This is a bug please report it with the following information: UNTRIGGERED_RETURN: [{}:{}]", file!(), line!())
+    }
+    fn eval_call(&mut self, call: &Node) -> Result<Value> {
+
+        if let NType::Ident(function) = &call.ntype {
+           
+            match function.as_str() {
+                _ => unimplemented!(),
+            }
+
+        } else {
+                error!("This is a bug, please report it with the following information: INVALID_IDENTIFIER: [{}:{}]", file!(), line!())
+        }
+
+    }
 }
