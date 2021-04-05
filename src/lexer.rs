37b7010d2a567978d07ad142699d141a4f41e049
Wafelack <wafelack@protonmail.com>
2021-04-05 16:25:03 +0200

    license: Added copyright notices.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2021, Wafelack <wafelack@protonmail.com>
+ *
+ *  ------------------------------------------------------
+ *
+ *     This file is part of Orion.
+ *
+ *  Orion is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Orion is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Orion.  If not, see <https://www.gnu.org/licenses/>.
+ */
 use crate::{error, OrionError, Result};
 
 #[derive(Clone, PartialEq, Debug)]
