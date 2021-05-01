1530c870bf2f91011328b91e545b7bc7f2e48541
Wafelack <wafelack@protonmail.com>
2021-05-01 23:00:23 +0200

    compiler: Add lambda compilation.

---
+++ src/bytecode.rs
@@ -0,0 +1,31 @@
+use crate::parser::Literal;
+
+#[derive(Copy, Clone, Debug)]
+pub enum OpCode {
+    Lambda(u16, u16), // Lambda(arg, chunk)
+    LoadConst(u16),
+    LoadSym(u16),
+    Call(u16),
+    Builtin(u8, u8),
+    Def(u16),
+    Add,
+}
+
+#[derive(Clone, Debug)]
+pub struct Bytecode {
+   pub chunks: Vec<Vec<OpCode>>,
+   pub symbols: Vec<String>,
+   pub constants: Vec<Literal>,
+   pub instructions: Vec<OpCode>,
+}
+
+impl Bytecode {
+    pub fn new() -> Self {
+        Self {
+            chunks: vec![],
+            symbols: vec![],
+            constants: vec![],
+            instructions: vec![],
+        }
+    }
+}
