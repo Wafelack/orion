fee8c56bdf19ff2f03cd3fc674c94ad712cf0eb1
Wafelack <wafelack@protonmail.com>
2021-05-06 10:55:41 +0200

    compiler: Fix lambda compilation.

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -5,17 +5,18 @@ pub enum OpCode {
     LoadConst(u16),
     LoadSym(u16),
     Call(u16),
-Builtin(u8, u8),
+    Builtin(u8, u8),
     Def(u16),
+    Lambda(u16),
     Add,
 }
 
 #[derive(Clone, Debug)]
 pub struct Bytecode {
-   pub chunks: Vec<Vec<OpCode>>,
-   pub symbols: Vec<String>,
-   pub constants: Vec<Literal>,
-   pub instructions: Vec<OpCode>,
+    pub chunks: Vec<Vec<OpCode>>,
+    pub symbols: Vec<String>,
+    pub constants: Vec<Literal>,
+    pub instructions: Vec<OpCode>,
 }
 
 impl Bytecode {
