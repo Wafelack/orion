5f843636e2d206d5b4a09d1098e219089b9e38a0
Wafelack <wafelack@protonmail.com>
2021-05-09 23:04:31 +0200

    compiler: Rewrite compiler and fix lambda (symbol table).

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,9 @@
-use crate::{Result, error, OrionError, parser::Literal, bytecode::{Bytecode, Chunk, OpCode}};
+use crate::{
+    bytecode::{Bytecode, Chunk, OpCode},
+    error,
+    parser::Literal,
+    OrionError, Result,
+};
 
 #[derive(Debug, Clone)]
 pub enum Value {
@@ -12,6 +17,7 @@ pub enum Value {
 pub struct VM<const STACK_SIZE: usize> {
     pub input: Bytecode,
     pub stack: Vec<Value>,
+    pub builtins: Vec<(fn(&mut VM<STACK_SIZE>) -> Result<()>, u8)>,
 }
 
 fn to_val(lit: &Literal) -> Value {
@@ -19,24 +25,52 @@ fn to_val(lit: &Literal) -> Value {
         Literal::Integer(i) => Value::Integer(*i),
         Literal::Single(s) => Value::Single(*s),
         Literal::String(s) => Value::String(s.to_string()),
-        Literal::Unit => Value::Unit
+        Literal::Unit => Value::Unit,
     }
 }
 
 impl<const STACK_SIZE: usize> VM<STACK_SIZE> {
     pub fn new(input: Bytecode) -> Self {
-        Self {
+        let mut to_ret = Self {
             input,
             stack: Vec::with_capacity(STACK_SIZE),
+            builtins: vec![],
+        };
+        to_ret.register_builtin(Self::add, 2);
+        to_ret
+    }
+    fn add(&mut self) -> Result<()> {
+        let lhs = self.stack.pop().unwrap();
+        let rhs = self.stack.pop().unwrap();
+
+        match lhs {
+            Value::Integer(lhs) => match rhs {
+                Value::Integer(rhs) => self.stack.push(Value::Integer(lhs + rhs)),
+                _ => return error!("Expected an Integer, found a {:?}.", rhs),
+            }
+            Value::Single(lhs) => match rhs {
+                Value::Single(rhs) => self.stack.push(Value::Single(lhs + rhs)),
+                _ => return error!("Expected a Single, found a {:?}.", rhs),
+
+            }
+            _ => return error!("Expected a Single or an Integer, found a {:?}.", lhs),
         }
+        Ok(())
+    }
+    fn register_builtin(&mut self, func: fn(&mut VM<STACK_SIZE>) -> Result<()>, argc: u8) {
+        self.builtins.push((func, argc))
     }
     fn eval_opcode(&mut self, opcode: OpCode, ctx: &mut Vec<Value>) -> Result<()> {
         match opcode {
             OpCode::LoadConst(id) => self.stack.push(to_val(&self.input.constants[id as usize])),
             OpCode::LoadSym(id) => self.stack.push(ctx[id as usize].clone()),
             OpCode::Def(sym_id) => {
-                assert_eq!(ctx.len() as u16, sym_id);
-                ctx.push(self.stack.pop().unwrap());
+                let popped = self.stack.pop().unwrap();
+                if sym_id as usize >= ctx.len() {
+                    ctx.push(popped);
+                } else {
+                    ctx[sym_id as usize] = popped;
+                }
             }
             OpCode::Lambda(chunk_id) => self.stack.push(Value::Lambda(chunk_id)),
             OpCode::Call(argc) => {
@@ -48,12 +82,24 @@ impl<const STACK_SIZE: usize> VM<STACK_SIZE> {
                 if let Value::Lambda(chunk) = func {
                     let chunk = &self.input.chunks[chunk as usize];
                     if chunk.symbols.len() != args.len() {
-                        return error!("Expected {} arguments, found {}.", chunk.symbols.len(), args.len());
+                        return error!(
+                            "Expected {} arguments, found {}.",
+                            chunk.symbols.len(),
+                            args.len()
+                            );
                     }
 
                     for idx in 0..chunk.symbols.len() {
-                        ctx.push(args[idx].clone());
+                        let val = args[idx].clone();
+                        let chunk_id = chunk.symbols[idx] as usize;
+                        if chunk_id >= ctx.len() {
+                            ctx.push(val);
+                        } else {
+                            ctx[chunk_id] = val;
+                        }
                     }
+
+                    println!("{:?}", &ctx);
 
                     for instr in chunk.instructions.clone() {
                         self.eval_opcode(instr, ctx)?;
@@ -62,22 +108,12 @@ impl<const STACK_SIZE: usize> VM<STACK_SIZE> {
                     return error!("Expected a Lambda, found a {:?}.", func);
                 }
             }
-            OpCode::Add => {
-                let lhs = self.stack.pop().unwrap();
-                let rhs = self.stack.pop().unwrap();
-
-                match lhs {
-                    Value::Integer(lhs) => match rhs {
-                        Value::Integer(rhs) => self.stack.push(Value::Integer(lhs + rhs)),
-                        _ => return error!("Expected an Integer, found a {:?}.", rhs),
-                    }
-                    Value::Single(lhs) => match rhs {
-                        Value::Single(rhs) => self.stack.push(Value::Single(lhs + rhs)),
-                        _ => return error!("Expected a Single, found a {:?}.", rhs),
-
-                    }
-                    _ => return error!("Expected a Single or an Integer, found a {:?}.", lhs),
+            OpCode::Builtin(idx, argc) => {
+                let (f, f_argc) = self.builtins[idx as usize];
+                if f_argc != argc {
+                    return error!("Builtin 0x{:02x} takes {} arguments, but {} arguments were supplied.", idx, f_argc, argc);
                 }
+                f(self)?;
             }
             _ => todo!(),
         }
@@ -85,7 +121,7 @@ impl<const STACK_SIZE: usize> VM<STACK_SIZE> {
         Ok(())
     }
     pub fn eval(&mut self) -> Result<Vec<Value>> {
-        let mut ctx= vec![];
+        let mut ctx = vec![];
         for instr in self.input.instructions.clone() {
             self.eval_opcode(instr, &mut ctx)?;
         }
