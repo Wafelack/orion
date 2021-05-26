76770da3693452eb558acddd4ac50ec424601dd1
wafelack <wafelack@protonmail.com>
2021-05-26 13:49:03 +0200

    builtins: Add format.

--- src/compiler.rs
+++ src/compiler.rs
@@ -61,6 +61,7 @@ impl Compiler {
         to_ret.register_builtin("atan", false);
 
         to_ret.register_builtin("show", false);
+        to_ret.register_builtin("format", false);
 
         to_ret.register_builtin("putStr", true);
         to_ret.register_builtin("getLine", true);
